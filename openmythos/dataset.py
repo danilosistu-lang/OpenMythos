b8d9cf742a98f0140daf889e352d7cb9b21d6844
Danilo Sistu <danilosistu@gmail.com>
2026-08-26 19:54:54 +0200

    Change default dataset to 'ProCreations/Ultra-FineWeb-EDU'

--- openmythos/dataset.py
+++ openmythos/dataset.py
@@ -36,7 +36,7 @@ import torch.utils.data as torch_data
 
 logger = logging.getLogger("openmythos.dataset")
 
-DEFAULT_DATASET = "HuggingFaceFW/fineweb-edu"
+DEFAULT_DATASET = "ProCreations/Ultra-FineWeb-EDU"
 
 
 # ===========================================================================
